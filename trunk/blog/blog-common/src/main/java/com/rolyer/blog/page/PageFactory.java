824d1df2ce9b201f58e5d1bb7bfed39ceec7a0d0
rolyer.live@gmail.com <rolyer.live@gmail.com@c19bf5aa-f246-0228-f96b-94a361dd43c3>
2011-11-02 09:32:06 +0000

    fix page

---
+++ trunk/blog/blog-common/src/main/java/com/rolyer/blog/page/PageFactory.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright 2010 ROLYER.
+ * All right reserved.
+ * Created on 2011-11-02
+ */
+package com.rolyer.blog.page;
+
+/**
+ * @author Rolyer (rolyer.live@gmail.com)
+ * 
+ */
+public interface PageFactory {
+    Page getPage(String pageName);
+}
